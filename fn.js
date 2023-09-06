9fc141bf44dc9cf621f10a3206dd9abf032750ae
boyon99 <bo_yon_99@naver.com>
2023-09-06 20:37:28 +0900

    feat: 테스트 실행해보기

--- fn.js
+++
@@ -1,6 +0,0 @@
-const fn = {
-  add: (a, b) => a + b,
-  makeUser: (name, age) => ({ name, age })
-}
-
-module.exports = fn
---
+++ src/test/add.test.js
@@ -0,0 +1,7 @@
+const add = require('../add')
+
+test('', () => {
+  // test code
+  expect(add(1, 2)).toBe(3);
+})
+
